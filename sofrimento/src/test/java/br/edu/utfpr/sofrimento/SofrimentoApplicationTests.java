c6bb7c9a7c20d7bd259ca2885c3d920335f2120b
garra400 <joaovicgomes.10@gmail.com>
2025-09-10 13:27:04 -0300

    feat: adicionado estrutura inicial do desenvolvimento

---
+++ sofrimento/src/test/java/br/edu/utfpr/sofrimento/SofrimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.sofrimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofrimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
