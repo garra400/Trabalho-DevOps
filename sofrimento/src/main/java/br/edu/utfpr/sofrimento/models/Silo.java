3c4ede67ac9717ffe6c94a43b78504fd95dff1bf
Marcos-UTFPR <marcosviniciussilva@alunos.utfpr.edu.br>
2025-09-10 14:19:42 -0300

    feat: Adicionado Silo.java e SiloRepository.java

---
+++ sofrimento/src/main/java/br/edu/utfpr/sofrimento/models/Silo.java
@@ -0,0 +1,16 @@
+package br.edu.utfpr.sofrimento.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter // Em cima da classe, cria os get e set automaticamente
+@Setter
+@ToString
+@Entity     //Indica que é uma entidade a ser preservada no database
+@Table(name="tb_silo") //Nome da tabela
+public class Silo extends BaseEntity{
+    private Integer physicalId;
+}
