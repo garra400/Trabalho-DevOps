0a9dc508230bb136d0eb5ccf952c6e461c48f538
Laerte64 <ledurmenegol@gmail.com>
2025-09-10 17:57:12 +0000

    fix: sensor

--- sofrimento/src/main/java/br/edu/utfpr/sofrimento/models/Sensor.java
+++ sofrimento/src/main/java/br/edu/utfpr/sofrimento/models/Sensor.java
@@ -2,19 +2,26 @@ package br.edu.utfpr.sofrimento.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+@Getter
+@Setter
+@ToString
 @Entity
 @Table(name = "tb_sensor")
 public class Sensor extends BaseEntity {
+    
     @Column(name = "physical_id", nullable = false)
     private String physicalID;
     @Column(name = "sensor_type", nullable = false)
     private SensorType sensorType;
+    
     @ManyToOne
-    @Column(nullable = false)
-    private Silo silo;
+    @JoinColumn(nullable = false)
+    private Device device;
 }
