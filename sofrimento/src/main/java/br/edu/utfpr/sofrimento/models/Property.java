cd31ff77a20acbb8f1e6dc88e3b69f37e97047fc
Bernard Moreno <bernard.moreno098@gmail.com>
2025-09-10 14:51:49 -0300

    feat: relação propriedade -> pessoa

--- sofrimento/src/main/java/br/edu/utfpr/sofrimento/models/Property.java
+++ sofrimento/src/main/java/br/edu/utfpr/sofrimento/models/Property.java
@@ -2,6 +2,7 @@ package br.edu.utfpr.sofrimento.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,4 +18,6 @@ public class Property extends BaseEntity{
     @Column(name = "name", length = 200, nullable = false)
     private String name;
 
+    @ManyToOne
+    private Person person;
 }
