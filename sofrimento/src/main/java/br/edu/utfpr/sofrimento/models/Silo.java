e2912a1fa5ff0520240806f1e885e956f62bb82e
Marcos-UTFPR <marcosviniciussilva@alunos.utfpr.edu.br>
2025-09-10 14:23:59 -0300

    feat: Adicionado @Column no atributo physicalId da classe Silo

--- sofrimento/src/main/java/br/edu/utfpr/sofrimento/models/Silo.java
+++ sofrimento/src/main/java/br/edu/utfpr/sofrimento/models/Silo.java
@@ -1,5 +1,6 @@
 package br.edu.utfpr.sofrimento.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -12,5 +13,6 @@ import lombok.ToString;
 @Entity     //Indica que é uma entidade a ser preservada no database
 @Table(name="tb_silo") //Nome da tabela
 public class Silo extends BaseEntity{
+    @Column(name = "physical_id", nullable=false)
     private Integer physicalId;
 }
