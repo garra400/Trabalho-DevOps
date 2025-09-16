63b84e8679abf8eab496b448295f17b1cd7cfbc1
Marcos-UTFPR <marcosviniciussilva@alunos.utfpr.edu.br>
2025-09-16 15:20:51 -0300

    Fix: Renomeado o método findByPersonID

--- sofrimento/src/main/java/br/edu/utfpr/sofrimento/repositories/SiloRepository.java
+++ sofrimento/src/main/java/br/edu/utfpr/sofrimento/repositories/SiloRepository.java
@@ -9,5 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.edu.utfpr.sofrimento.models.Silo;
 
 public interface SiloRepository extends JpaRepository<Silo, UUID> {
-    Page<Silo> findByPessoaId(UUID pessoaId, Pageable pageable);
+    Page<Silo> findByPersonId(UUID personId, Pageable pageable);
 }
