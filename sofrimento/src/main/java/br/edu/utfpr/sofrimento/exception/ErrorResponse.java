b65b436019b3918f2d44c52811c8396d90aa483c
Marcos-UTFPR <marcosviniciussilva@alunos.utfpr.edu.br>
2025-09-16 15:01:32 -0300

    Feat: Adicionado pasta Exception e exceção customizada NotFoundException

---
+++ sofrimento/src/main/java/br/edu/utfpr/sofrimento/exception/ErrorResponse.java
@@ -0,0 +1,9 @@
+package br.edu.utfpr.sofrimento.exception;
+
+/**
+ * Classe para representar uma resposta de erro.
+ * Ela contém uma mensagem descritiva do erro ocorrido.
+ */
+public record ErrorResponse(String message) {
+    
+}
