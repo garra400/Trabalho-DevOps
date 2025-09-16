ed127fb83ffcaabc54fea02d2627e6780492eb6c
Marcos-UTFPR <marcosviniciussilva@alunos.utfpr.edu.br>
2025-09-16 15:18:38 -0300

    Feat: Atualizado SiloRepository

--- sofrimento/src/main/java/br/edu/utfpr/sofrimento/repositories/SiloRepository.java
+++ sofrimento/src/main/java/br/edu/utfpr/sofrimento/repositories/SiloRepository.java
@@ -2,10 +2,12 @@ package br.edu.utfpr.sofrimento.repositories;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.utfpr.sofrimento.models.Silo;
 
 public interface SiloRepository extends JpaRepository<Silo, UUID> {
-    // Obter?
+    Page<Silo> findByPessoaId(UUID pessoaId, Pageable pageable);
 }
