11127f875b30c50772eaf3a07989758a31140306
garra400 <joaovicgomes.10@gmail.com>
2025-09-17 13:32:10 -0300

    feat: adicionado interface DeviceRepository

--- sofrimento/src/main/java/br/edu/utfpr/sofrimento/repositories/DeviceRepository.java
+++ sofrimento/src/main/java/br/edu/utfpr/sofrimento/repositories/DeviceRepository.java
@@ -2,11 +2,12 @@ package br.edu.utfpr.sofrimento.repositories;
 
 import java.util.UUID;
 
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.utfpr.sofrimento.models.Device;
 
 public interface DeviceRepository extends JpaRepository<Device, UUID> {
-
+    Page<Device> findBySiloId(UUID siloId, Pageable pageable);
 }
-
