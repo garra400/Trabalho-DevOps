7dc7843a9a1beb7029dcbd5cae9e92e4d5642db8
Bernard Moreno <bernard.moreno098@gmail.com>
2025-09-17 14:41:35 -0300

    feat: controller, service e repository de property criados

--- sofrimento/src/main/java/br/edu/utfpr/sofrimento/repositories/PropertyRepository.java
+++ sofrimento/src/main/java/br/edu/utfpr/sofrimento/repositories/PropertyRepository.java
@@ -2,10 +2,12 @@ package br.edu.utfpr.sofrimento.repositories;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.utfpr.sofrimento.models.Property;
 
 public interface PropertyRepository extends JpaRepository<Property, UUID>{
-
+    Page<Property> findByPersonId(UUID personId, Pageable pageable);
 }
